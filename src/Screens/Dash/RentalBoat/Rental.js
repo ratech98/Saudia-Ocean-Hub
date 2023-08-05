f87270f67f1ced8d5742420dd9b130e10b7caab0
GJ <riyaz@amicusinfotech.com>
2023-08-05 16:53:02 +0530

    navi issuse fixed

--- src/Screens/Dash/RentalBoat/Rental.js
+++ src/Screens/Dash/RentalBoat/Rental.js
@@ -94,7 +94,7 @@ export const Rental = () => {
         handleExtraInputChange={handleExtraInputChange}
         presentPage={"Home"}
         homeBtn={"Home"}
-        homeBtnHref={"/rental"}
+        homeBtnHref={auth?.AuthToken ? "/rental" : "/"}
         link1={link1}
         link2={link2}
         link3={link3}
